ad38be22a1ffdf426831084213b4759b7544ede3
Vladimir Parfionov <jobstest18@gmail.com>
2022-09-04 18:07:08 +0400

    added TestAllure

---
+++ src/test/java/com/gmail/jobstest18/LabelsTest.java
@@ -0,0 +1,19 @@
+package com.gmail.jobstest18;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LabelsTest {
+
+    @Test
+    @Owner("parfionov")
+    @DisplayName("Test")
+    @Severity(SeverityLevel.BLOCKER)
+    @Feature("Задачи в репозитории") // то что отделяемо
+    @Story("Просмотр созданных задач в репозитории")
+    @Link(value = "Страница регистрации", url = "https://demoqa.com/automation-practice-form")
+    public void testAnnotated() {
+
+    }
+}
